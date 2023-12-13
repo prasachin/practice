0c07592febee229ac7623bea47ff35cea08fc41f
sachin <sachinp@iitbhilai.ac.in>
2023-12-13 20:23:25 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const jsonServer=require("json-server");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router=jsonServer.router("db.json");
-const middlewares=jsonServer.defaults();
-const PORT=process.env.PORT || 8080;
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; 
+
 server.use(middlewares);
 server.use(router);
-server.listen(PORT);
+
+server.listen(port);
